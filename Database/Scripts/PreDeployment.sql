2c14c6632eb64612f014b565465421a0e7cd9be7
Kseniya Laurynovich <KseniyaLaurynovich@gmail.com>
2016-11-07 18:59:51 +0300

    added file table

---
+++ Database/Scripts/PreDeployment.sql
@@ -0,0 +1,13 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+add filegroup FileGroup_Projects contains filestream;
+go
+
+ALTER DATABASE [$(DatabaseName)]
+ SET FILESTREAM ( NON_TRANSACTED_ACCESS = FULL, DIRECTORY_NAME = 'FirstStep_Projects' )  
+go
+
+ALTER DATABASE [$(DatabaseName)]
+ADD FILE
+  (NAME = 'FileTable', FILENAME = 'D:\FileStream\SMT')
+to filegroup FileGroup_Projects;
+go
