d05fdf0d97783dcd69f3ca1db10e0c4861eddf2a
KseniyaLaurynovich <kseniyalaurynovich@gmail.com>
2016-11-28 23:22:34 +0300

    added contracts

---
+++ FirstStep/BusinesServices/Contracts/IGroupService.cs
@@ -0,0 +1,14 @@
+﻿using BusinesServices.Models;
+
+namespace BusinesServices.Contracts
+{
+    public interface IGroupService
+    {
+        void Save(Group group);
+        void Delete(string id);
+        void GetById(string id);
+        void AssignUser(string userId);
+        void AssignUsers(string[] usersIds);
+        void RemoveFromGroup(string userId);
+    }
+}
